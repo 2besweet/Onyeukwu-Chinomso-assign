ed1bde54a83887229c93cdc6c5e8eccd57a5a342
Chinomso <chichinomso@gmail.com>
2022-07-15 18:36:46 +0100

    new

--- index.js
+++ index.js
@@ -19,12 +19,13 @@ PS: The solution to this algorithm challenge must be written in either Javascrip
 function twoSum(arr, target) {
     for(let i=0; i < arr.length; i++){
         for(let j = i + 1; j < arr.length; j++){
-            if(arr[i] + arr[j] === target) {
+            if(arr[i] + arr[j] == target) {
                 return[i,j]
             }
         }
     }
 }
+
 
 const nums1 =[2,7,11,15]
 
